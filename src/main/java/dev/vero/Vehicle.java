98aff1823ec40be91939ca23b4a20946f2a3f429
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-04 21:34:07 +0200

    vehicle class done

---
+++ src/main/java/dev/vero/Vehicle.java
@@ -0,0 +1,19 @@
+package dev.vero;
+
+public abstract class Vehicle {
+private String licensePlate;
+
+public Vehicle(String licensePlate) {
+    this.licensePlate = licensePlate;
+}
+
+public String getLicensePlate() {
+    return licensePlate;
+}
+
+public void setLicensePlate(String licensePlate) {
+    this.licensePlate = licensePlate;
+}
+
+public abstract double calculateBill();
+}
