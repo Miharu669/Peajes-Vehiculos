529f5e101ac8169b0bf305805112d74c270eff50
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-05 00:04:50 +0200

    added more test to tollstation

--- src/test/java/dev/vero/TollStationTest.java
+++ src/test/java/dev/vero/TollStationTest.java
@@ -13,6 +13,7 @@ public class TollStationTest {
     @Test
     public void testTollStation() {
         TollStation tollStation = new TollStation("West Gate", "Madrid");
+
         assertEquals("West Gate", tollStation.getName());
         assertEquals("Madrid", tollStation.getCity());
         assertEquals(0.0, tollStation.getTollCollected(), 0.01);
@@ -27,6 +28,7 @@ public class TollStationTest {
 
         tollStation.addVehicle(car1);
         tollStation.addVehicle(car2);
+
         assertEquals(2, tollStation.getVehicles().size());
         assertTrue(tollStation.getVehicles().contains(car1));
         assertTrue(tollStation.getVehicles().contains(car2));
@@ -47,11 +49,57 @@ public class TollStationTest {
     }
 
     @Test
-    public void testGetTollCollected(){
+    public void testGetTollCollected() {
         TollStation tollStation = new TollStation("South Gate", "Asturias");
         Truck truck1 = new Truck("1234XYZ", 6);
+
         tollStation.addVehicle(truck1);
+
+        assertEquals(300, tollStation.getTollCollected(), 0.01);
+    }
+
+    @Test
+    public void testGetTolLCollectedWithCar() {
+        TollStation tollStation = new TollStation("South Gate", "Asturias");
+        Car car1 = new Car("ABC123");
+
+        tollStation.addVehicle(car1);
+
+        assertEquals(100, tollStation.getTollCollected(), 0.01);
+    }
+
+    @Test
+    public void testGetTolLCollectedWithMotorcycle() {
+        TollStation tollStation = new TollStation("South Gate", "Asturias");
+        Motorcycle motorcycle1 = new Motorcycle("DEF456");
+
+        tollStation.addVehicle(motorcycle1);
+
         assertEquals(50, tollStation.getTollCollected(), 0.01);
+    }
+
+    @Test
+    public void testGetTolLCollectedWithTruck() {
+        TollStation tollStation = new TollStation("South Gate", "Asturias");
+        Truck truck1 = new Truck("1234XYZ", 6);
+
+        tollStation.addVehicle(truck1);
+
+        assertEquals(300, tollStation.getTollCollected(), 0.01);
+    }
+
+    @Test
+    public void testPrintReport() {
+        TollStation tollStation = new TollStation("South Gate", "Asturias");
+        Car car1 = new Car("ABC123");
+        Motorcycle motorcycle1 = new Motorcycle("DEF456");
+        Truck truck1 = new Truck("1234XYZ", 6);
+
+        tollStation.addVehicle(car1);
+        tollStation.addVehicle(motorcycle1);
+        tollStation.addVehicle(truck1);
+
+        tollStation.printReport();
     }
 
 }
