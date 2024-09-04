534205a7813bc4e9ed2b7373a9f6e462adcf71ed
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-04 21:47:05 +0200

    Truck class added

---
+++ src/main/java/dev/vero/Truck.java
@@ -0,0 +1,26 @@
+package dev.vero;
+
+public class Truck extends Vehicle { 
+    
+    private static final double TRUCK_RATE = 50;
+
+    private int numAxis;
+
+    public Truck(String licensePlate, int numAxis) {
+        super(licensePlate);
+        this.numAxis = numAxis;
+    }
+
+    public int getNumAxis() {
+        return numAxis;
+    }
+
+    public void setNumAxis(int numAxis) {
+        this.numAxis = numAxis;
+    }
+
+    @Override
+    public double calculateBill() {
+        return TRUCK_RATE;
+    }
+}
